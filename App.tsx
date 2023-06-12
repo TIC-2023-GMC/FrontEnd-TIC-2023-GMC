530000e88253c1ba145abdb68b2c19ced116b457
Erickneitor1416 <erick.munoz@epn.edu.ec>
2023-06-11 19:46:46 -0500

    add Eslint and prettier config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import HelloWord from "./components/HelloWord";
+import HelloWord from "./src/components/HelloWord";
 
 
 
--- services/api.tsx
+++
@@ -1,6 +0,0 @@
-import axios, { InternalAxiosRequestConfig } from "axios";
-import Constants from "expo-constants";
-
-export const baseUrl = Constants.manifest?.extra?.apiUrl;
-axios.defaults.baseURL = baseUrl;
-export const { get, post, put, delete: del, patch } = axios;
