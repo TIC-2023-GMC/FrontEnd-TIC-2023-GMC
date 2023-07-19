4ade8c5ca0037217078c52b31cfa565e49d1fd1a
KFernandoCaranquiY <kevin.caranqui@epn.edu.ec>
2023-07-19 01:38:52 -0500

    PAWQ-56: Add PhotoSelection Component

--- src/components/PhotoSelection.tsx
+++ src/components/PhotoSelection.tsx
@@ -1,35 +1,46 @@
-import  React, {useState} from 'react'
-import { FlatList, Image, View } from 'react-native';
-import { Button } from 'react-native';
-import * as ImagePicker from 'expo-image-picker'
+import React, { useState } from 'react';
+import { StyleSheet, Image, View } from 'react-native';
+import { Button, IconButton } from 'react-native-paper';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function PhotoSelection(){
-    const [images, setImages]= useState([]);
-
-    const pickImages = async () =>{
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: true,
-            selectionLimit: 1,
-            aspect:[4,3],
-            quality:1
-        })
-    };
-    return(
-        <FlatList
-        data = {images}
-        renderItem= {({item}) =>(
-            <Image
-            source={{uri: item.uri}}
-            style={{width:430, height:250}}
-            />
-        )}
-        keyExtractor={(item)=>item.uri}
-        ListHeaderComponent={
-            <View>
-                <Button title="Pick images" onPress={pickImages} />
-            </View>
-        }
-        />
-    )
+interface PhotoSelectionProps {
+	image: ImagePicker.ImagePickerResult | undefined;
+	setImage: React.Dispatch<React.SetStateAction<ImagePicker.ImagePickerResult | undefined>>;
 }
+
+export default function PhotoSelection({ image, setImage }: PhotoSelectionProps) {
+	const pickImages = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			selectionLimit: 1,
+			aspect: [4, 3],
+			quality: 1
+		});
+		if (!result.canceled) {
+			setImage(result);
+		}
+	};
+	return (
+		<View style={styles.container}>
+			<Image source={{ uri: image?.assets?.pop()?.uri }} style={styles.image} />
+			<IconButton onPress={pickImages} size={30} icon="image-plus" mode="contained-tonal" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		marginTop: 10,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	image: {
+		width: 240,
+		height: 180,
+		borderWidth: 1,
+		borderColor: '#000000',
+		borderRadius: 10
+	}
+});
+
