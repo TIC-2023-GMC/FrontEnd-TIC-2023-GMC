e27f8e5408c867665e01ab65a523ec1aba55bc9b
Kevin Caranqui <kevin.caranqui@epn.edu.ec>
2023-07-24 21:25:44 -0500

    PAWQ-52: File creation

---
+++ src/Screens/Game/MenuGameScreen.tsx
@@ -0,0 +1,10 @@
+export default function MenuGameScreen() {
+    return (
+        <>
+        {/* Sección para la foto logo del juego*/}
+        {/* Sección para las cartitas (botones) */}
+        {/*  */}
+        </>
+
+    )
+}
