e837b7f95f6467c5dbd0ed986d86a7f4c95feb2f
Gandhy <gandhy.garcia@epn.edu.ec>
2023-07-26 23:49:56 -0500

    PAWQ-36: Fix zod schema for ExperiencePubliactionSchema (date)

--- src/models/Schemas.ts
+++ src/models/Schemas.ts
@@ -58,7 +58,7 @@ export const ExperiencePublicationSchema = z.object({
 	_id: z.string(),
 	user: UserSchema,
 	description: z.string().nonempty('La descripción es requerida'),
-	publication_date: z.string(),
+	publication_date: z.date(),
 	photo: PhotoSchema,
 	likes: z.optional(z.array(Like)),
 	comments: z.optional(z.array(Comment)),
